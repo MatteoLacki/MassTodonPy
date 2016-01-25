e2ed9397f08ba1ac40bf52df976cc4a4b94b80dd
Mateusz Łącki <matteo.lacki@gmail.com>
2016-01-25 12:11:33 +0100

    Bonds tagging online.

--- formulaGenerator/proteins.py
+++ formulaGenerator/proteins.py
@@ -10,20 +10,16 @@ def aminosIter(fasta):
 	aas 	= ('A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V')
 	AAS 	= {}
 	for aa in aas:
-		A = AminoAcid(aa)
-		AAS[aa] = ( A.getLeft(), A.getRight(), A.getGraph() )
+		AAS[aa] = AminoAcid(aa)
 	for i,aa in enumerate(fasta):
-		
-		if i==0:
+		if i in (0, len(fasta)-1):
 			A = AminoAcid(aa)
-			A.add_H()
-			yield ( A.getLeft(), A.getRight(), A.getGraph() )
-		elif i < len(fasta)-1:
-			yield AAS[aa]
+			if i == 0:
+				A.add_H()
+			if i == len(fasta)-1:
+				A.add_OH()
 		else:
-			A = AminoAcid(aa)
-			A.add_OH()
-
+			A = AAS[aa]
 		yield ( A.getLeft(), A.getRight(), A.getGraph() )
 
 def edges(fasta):
@@ -40,7 +36,7 @@ def edges(fasta):
 def BondTypes(bonds = ['cz']):
 	B2atoms = {	'cz': [ ('Nalpha', 'Calpha'), ('N1', 'C2') ],
 				'by': [ ('Nalpha', 'Ccarbo'), ('N1', 'Ccarbo') ],
-				'ax': [ ('Calpha','Ccarbo'), ('N1', 'Ccarbo') ]  	}
+				'ax': [ ('Calpha','Ccarbo'), ('C2', 'Ccarbo') ]  	}
 	B2B = defaultdict(lambda: None)
 	try: 
 		for b in bonds:
@@ -50,22 +46,19 @@ def BondTypes(bonds = ['cz']):
 	except KeyError:
 		print 'This type of bond is yet not considered: '+str(b)
 
-B2B = BondTypes()
-
-def bonds(fasta, B2B):
+def GetBonds(fasta, B2B):
 	bondTypes= set(B2B[k] for k in B2B)
 	notFirst = False
 	for left, right, G in aminosIter(fasta):
 		for e in G.es:
 			yield B2B[ tuple(vName for vName in G.vs[e.tuple]['name']) ]
-		if 'by' in bondTypes:
-			if notFirst:
+		if notFirst:
+			if 'by' in bondTypes:
 				yield 'by'
 			else:
-				notFirst = True
-				yield None
-		else:
-			yield None
+				yield None			
+		else:	
+			notFirst = True
 
 def propGen(propName, fasta):
 	if propName == 'name':
@@ -73,73 +66,22 @@ def propGen(propName, fasta):
 	else:
 		return ( pN for _,_,G in aminosIter(fasta) for pN in G.vs[propName] )
 
-def makeProtein( fasta, attributeNames=('name', 'elem') ):
+def makeProtein( fasta, attributeNames=('name', 'elem'), bonds=['cz','ax','by'] ):
 	N 	= sum( len(G.vs) for l,r,G in aminosIter(fasta) )
-	B2B = BondTypes()
+	B2B = BondTypes(bonds)
 	G 	= ig.Graph(N)
 	G.add_edges( edges(fasta) )
 	for name in attributeNames:
 		G.vs[name] = [ a for a in propGen(name, fasta) ]
-	G.es['Roep'] = list(bonds(fasta,B2B))
+	G.es['Roep'] = list(GetBonds(fasta,B2B))
 	return G
 
 
-
-fasta = substanceP
-fasta = 'AA'
-G = makeProtein(fasta)		
-print G
-G.es['Roep']
+fasta 	= substanceP
+G 		= makeProtein(fasta, bonds=['cz','by'])		
 plott(G, edge_label=G.es['Roep'], bbox=(2000,1000))
 
 
-
-for vs in G.vs['name']:
-
-G.vs([0,2,4])['name']
-
-G.vs.select
-
-
-
-vertices
-
-
-G= {'v1': ['v2', 'v3'], 'v2': ['v1'], 'v3': ['v1', 'v4'], 'v4': ['v3']}
-mvi= {'v1': 1, 'v2': 2, 'v3': 3, 'v4': 4}
-graph= ig.Graph(edges= [(mvi[v], mvi[a]) for v in G.keys() for a in G[v]])
-print graph
-
-x.ig.Graph(
-	edges 
-)
-
-# Try using that.
-x = ig.Graph.DictList(
-	vertices=[{'name':'A'}, {'name':'B'}], 
-	edges 	=[{'s':'A', 't':'B', 'name': 'Hello'}],
-	vertex_name_attr = 'name',
-	edge_foreign_keys= ('s','t')
-)
-print x
-
-G.get_eid('9:Ccarbo','10:Nalpha')
-print G
-
-for i in G.es[1:10]:
-	print i.tuple
-
-for i,s in enumerate(fasta):
-	if s=='P':
-
-	else:
-
-
-
-
-# print G
-# plott(G, bbox=(1000,1000))
-
-ends, __,starts = G.bfs(0)
+# ends, __,starts = G.bfs(0)
 
 
