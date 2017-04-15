35c4522a7d418dc8cc3ea3dd4c61c74f18aced50
Matteo Lacki <matteo.lacki@gmail.com>
2017-04-15 17:51:46 +0200

    Ready for submission.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from distutils.core import setup
 setup(
     name          = 'MassTodonPy',
     packages      = ['MassTodonPy'], # this must be the same as the name above
-    version       = '0.01',
+    version       = '1.0',
     description   = 'A module that investigates the products of Electron Transfer Dissociation in Mass Spectrometry for a given biological substance.',
     author        = 'Mateusz Lacki',
     author_email  = 'matteo.lacki@gmail.com',
