401e6941b233516d6d690b1af9565853f2a1a98b
Mateusz Łącki <matteo@Mateuszs-MacBook-Pro-2.local>
2017-04-15 19:28:55 +0200

    test v 0.2 release

--- setup.py
+++ setup.py
@@ -16,19 +16,20 @@
 #   Version 3 along with MassTodon.  If not, see
 #   <https://www.gnu.org/licenses/agpl-3.0.en.html>.
 
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup, find_packages
 
 # I have no idea what I am doing...
 setup(
     name          = 'MassTodonPy',
     # packages      = ['MassTodonPy', 'MassTodonPy.data', 'MassTodonPy.Deconvolutor'], # this must be the same as the name above
     packages      = find_packages(),
-    version       = '1.0',
+    version       = '0.2',
     description   = 'A module that investigates the products of Electron Transfer Dissociation in Mass Spectrometry for a given biological substance.',
     author        = 'Mateusz Lacki',
     author_email  = 'matteo.lacki@gmail.com',
-    url           = 'https://MatteoLacki@bitbucket.org/MatteoLacki/masstodonpy',
-    download_url  = 'https://MatteoLacki@bitbucket.org/MatteoLacki/masstodonpy.git',
+    url           = 'https://github.com/MatteoLacki/MassTodonPy',
+    download_url  = 'https://github.com/MatteoLacki/MassTodonPy/archive/0.2.tar.gz',
     keywords      = ['Mass Spectrometry', 'ETD', 'Electron Transfer Dissociation', 'Fragmentation'],
     classifiers   = [],
 )
