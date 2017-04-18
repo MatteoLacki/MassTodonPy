62ac51b9eb25364fc4d6f2b121afea35abb136b0
Mateusz Łącki <matteo.lacki@gmail.com>
2017-04-18 11:51:48 +0200

    Have it!

--- Tests/export_data_from_R_structure.R
+++ Tests/export_data_from_R_structure.R
@@ -1,47 +1,38 @@
-suppressPackageStartupMessages(library("tidyverse"))
-suppressPackageStartupMessages(library("jsonlite"))
+library("jsonlite")
+library("tidyverse")
 
 P = function(x, l=1) str(x,max.level=l)
 getSpectrum = function(spec)
   spec %>% bind_rows %>% tbl_df %>% select(mz, intensity) %>% arrange(mz)
 
 load('/Users/matteo/Dropbox/MassTodon/ProcessedData/SubstanceP/synapt_WH_WV_substanceP_010513.RData')
-
 # P(data4plots)
 # P(data4plots$WH)
 # data4plots$WH$Data
 # P(data4plots$WH$MassTodons)
 # X <- data4plots$WH$MassTodons[[1]]
 # P(X)
-# Y <- X[[1]]
-# Y$formulas
-# Y$cTerminus
-# length(Y$nTerminus)
+# P(Y)
+# data4plots$WH$parsedFiles
+# P(data4plots$WH)
 
-getTerminalCnt <- function(terminus){
-  if( length(terminus) > 0 )
-      return(data.frame(terminus) %>% gather('Element','Count',1:ncol(.)))
-  else
-      return(NULL)
-}
-
-saveTerminalCnt <- function(terminus, path, i, type){
-  term <- getTerminalCnt(terminus)
-  if( !is.null(term) ){
-      write_csv( term, path=paste0(path,i,'_', type,'_terminal.txt'), col_names=FALSE )
-  }
-}
-
-# get other data and prepare a MetaInfo file with rows corresponding to i of a given spectrum.
-storagePath = '/Users/matteo/Documents/MassTodon/Data/Storage/'
-
-DATA <- list()
-
+DATA = list()
+i = 1
 for(d_cnt in 1:length(data4plots)){
-  X <- data4plots[[d_cnt]]$MassTodons
-  for(exp_cnt in 1:length(X)){
-    Y <- X[[exp_cnt]]
-    mass_spectrum <- getSpectrum( Y$spectrum )
-    write_csv( mass_spectrum, path=paste0(storagePath,i,'.txt'), col_names=FALSE )
-  }
+    X <- data4plots[[d_cnt]]$MassTodons
+    for(exp_cnt in 1:length(X)){
+        Y <- X[[exp_cnt]]
+        DATA[[i]] = Y[c('precursor','maxProtonsNo')]
+        DATA[[i]]$instrumental_setting = names(X)[exp_cnt]
+        DATA[[i]]$mass_spectrum = getSpectrum( Y$spectrum )
+        if( length(Y$cTerminus)>0 )
+            DATA[[i]]$cTerminus = Y$cTerminus
+        if( length(Y$nTerminus)>0 )
+            DATA[[i]]$nTerminus = Y$nTerminus
+        i = i+1
+    }
 }
+
+storagePath = '/Users/matteo/Documents/MassTodon/Data/Storage/'
+# pryr::object_size(toJSON(DATA,digits=10))
+write_json(DATA, path='/Users/matteo/Documents/MassTodon/MassTodonPy/Tests/spectra.json', digits=10)
