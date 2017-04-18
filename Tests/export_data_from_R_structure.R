c35a55a711baeb0d5777bffa0aca018cff08ef0b
Mateusz Łącki <matteo.lacki@gmail.com>
2017-04-18 10:50:48 +0200

    Preparing to make history!

---
+++ Tests/export_data_from_R_structure.R
@@ -0,0 +1,52 @@
+suppressPackageStartupMessages(library("tidyverse"))
+suppressPackageStartupMessages(library("jsonlite"))
+
+P = function(x, l=1) str(x,max.level=l)
+getSpectrum = function(spec)
+  spec %>% bind_rows %>% tbl_df %>% select(mz, intensity) %>% arrange(mz)
+
+load('/Users/matteo/Dropbox/MassTodon/ProcessedData/SubstanceP/synapt_WH_WV_substanceP_010513.RData')
+
+# P(data4plots)
+# P(data4plots$WH)
+# data4plots$WH$Data
+# P(data4plots$WH$MassTodons)
+# X <- data4plots$WH$MassTodons[[1]]
+# P(X)
+# Y <- X[[1]]
+# Y$formulas
+# Y$cTerminus
+# length(Y$nTerminus)
+
+getTerminalCnt <- function(terminus){
+  if( length(terminus) > 0 )
+      return(data.frame(terminus) %>% gather('Element','Count',1:ncol(.)))
+  else
+      return(NULL)
+}
+
+saveTerminalCnt <- function(terminus, path, i, type){
+  term <- getTerminalCnt(terminus)
+  if( !is.null(term) ){
+      write_csv( term, path=paste0(path,i,'_', type,'_terminal.txt'), col_names=FALSE )
+  }
+}
+
+# get other data and prepare a MetaInfo file with rows corresponding to i of a given spectrum.
+i = 0
+storagePath = '/Users/matteo/Documents/MassTodon/Data/Storage/'
+
+
+
+for(d_cnt in 1:length(data4plots)){
+  X <- data4plots[[d_cnt]]$MassTodons
+  for(exp_cnt in 1:length(X)){
+    Y <- X[[exp_cnt]]
+    mass_spectrum <- getSpectrum( Y$spectrum )
+    write_csv( mass_spectrum, path=paste0(storagePath,i,'.txt'), col_names=FALSE )
+    saveTerminalCnt(Y$cTerminus, storagePath, i, 'c')
+    saveTerminalCnt(Y$nTerminus, storagePath, i, 'n')
+
+    i = i+1
+  }
+}
---
+++ Tests/test_advanced_matchmaker.py
@@ -0,0 +1,51 @@
+%load_ext autoreload
+%load_ext line_profiler
+%autoreload
+
+
+from   MassTodonPy import MassTodon
+from   MassTodonPy.MatchMaker import reaction_analist_advanced, reaction_analist_basic, reaction_analist_upper_intermediate
+from   math import log, lgamma, log10, exp, sqrt
+import cPickle as pickle
+
+file_path = '/Users/matteo/Documents/MassTodon/Tests/'
+file_name = 'FRL_220715_ubi_952_ETD_40ms_04.mzXML'
+# file_name = 'Ubiquitin_ETD_10_ms_1071.matteo'
+
+fasta = 'MQIFVKTLTGKTITLEVEPSDTIENVKAKIQDKEGIPPDQQRLIFAGKQLEDGRTLSDYNIQKESTLHLVLRLRGG'
+Q=8; jP=.999; mzPrec=.05; precDigits=2; M_minProb=.7
+
+M = MassTodon(  fasta           = fasta,
+                precursorCharge = Q,
+                precDigits      = precDigits,
+                jointProbability= jP,
+                mzPrec          = mzPrec )
+
+# modifications = {   ('N',2) :       {'H': 1, 'O': +2, 'N': +3},
+#                     ('Calpha',2) :  {'H': 1, 'O': +2, 'N': +3},
+#                     ('Calpha',5) :  {'H': 2, 'S': +2, 'N': +2},
+#                     ('C',6) :       {'H': 2, 'S': +2, 'N': +200} }
+
+path = file_path+file_name
+cutOff = 100; topPercent = .999
+M.readSpectrum(path=path, cutOff=cutOff, digits=precDigits, topPercent=topPercent)
+M.prepare_problems(M_minProb)
+mu=1e-5; lam=0.0; nu=0.001
+
+# %%time
+res = M.run(solver='sequential', method='MSE', mu=mu, lam=lam, nu=0.001)
+MassTodonResults = res
+# with open(file_path, 'w') as f:
+    # pickle.dump(res, f)
+# with open(file_path, 'rb') as f:
+#     MassTodonResults = pickle.load(f)
+
+%%time
+LogProb = reaction_analist_advanced(MassTodonResults, Q, fasta, maxIter=100, const=1000, eps = 0.0, verbose=True)
+
+
+%%time
+EasyRes = reaction_analist_basic(MassTodonResults, Q, fasta)
+
+EasyRes['PTR']
+EasyRes['ETnoD']
