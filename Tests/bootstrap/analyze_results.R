f6e99815a0b31435d257069a0e9a065ef7ace520
Mateusz Łącki <matteo.lacki@gmail.com>
2017-05-22 15:58:28 +0200

    finished the stupid plot

--- Tests/bootstrap/analyze_results.R
+++ Tests/bootstrap/analyze_results.R
@@ -9,7 +9,76 @@ D = bind_rows(R,S)
 WV_300_D = D$prob %>% select(WH, WV, algo, ETnoD, PTR, real_or_sim, ID) %>% filter(WV==300)
 WV_300_D %>% filter(real_or_sim=='real', WH==150, WV==300)
 
-make_plot = function(D, x_arm="WV" ){
+make_box_plot = function(D, xlab_label, x_choice, x) D %>% 
+  filter_(x_choice, "count_or_prob=='prob'", "real_or_sim=='sim'") %>%
+  select(WH, WV, algo, ETnoD, PTR, ID, anion_approached_cation) %>%
+  mutate(WH = factor(WH), WV = factor(WV) ) %>%
+  mutate_(X=x) %>%
+  ggplot(aes(x=X, y=ETnoD, fill=algo, alpha =anion_approached_cation)) +
+  geom_boxplot() +
+  theme_minimal() +
+  xlab(xlab_label)+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1),
+        legend.position = 'top' ) +
+  scale_y_continuous(labels=scales::percent) 
+
+make_react_plot = function(D, xlab_label, x_choice, x) D %>% 
+  filter_(x_choice, "count_or_prob=='prob'", "real_or_sim=='sim'") %>%
+  select(WH, WV, algo, ETnoD, PTR, ID, anion_approached_cation) %>%
+  mutate(WH = factor(WH), WV = factor(WV) ) %>%
+  mutate_(X=x) %>%
+  ggplot(aes(x=X, y=anion_approached_cation)) +
+  geom_boxplot()+
+  theme_minimal() +
+  xlab(xlab_label)+
+  ylab('Probability of Anion Reaction') +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_y_continuous(labels=scales::percent) 
+  
+quant = function(vals, prob) 
+  if(any(is.na(vals))) return(NA) else return(quantile(vals, probs=prob)) 
+
+make_error_plot = function(D, xlab_label, x_choice, x, min_prob, max_prob){
+  LD= D %>% filter_(x_choice, "count_or_prob=='prob'", "real_or_sim=='sim'") %>%
+    select(WH, WV, algo, ETnoD, PTR, ID, anion_approached_cation) %>%
+    mutate(WH = factor(WH), WV = factor(WV) ) %>%
+    mutate_(X=x) %>%
+    group_by(X, algo) %>%
+    summarize(mean_val = mean(ETnoD),
+              prob_max = quant(ETnoD, max_prob),
+              prob_min = quant(ETnoD, min_prob) ) %>% ungroup
+  RD = D %>% filter_(x_choice, "count_or_prob=='prob'", "real_or_sim=='real'") %>%
+    select(WH, WV, algo, ETnoD, PTR, ID, anion_approached_cation) %>%
+    mutate(WH = factor(WH), WV = factor(WV) ) %>%
+    mutate_(X=x) %>%
+    group_by(X, algo) 
+  ggplot(LD) +
+    geom_crossbar(
+      aes(x = X,
+          y = mean_val,
+          ymin = prob_min, 
+          ymax = prob_max,
+          color = algo), 
+      width = 0.4,
+      position = position_dodge()
+    )+
+    geom_point(
+      data = RD,
+      aes(
+        x = X,
+        y = ETnoD
+      )
+    ) +
+    theme_minimal() +
+    xlab(xlab_label)+
+    ylab('ETnoD Bootstrap Estimates')+
+    theme(
+      axis.text.x = element_text(angle = 90, hjust = 1),
+      legend.position = 'top'
+    ) +
+    scale_y_continuous(labels=scales::percent)  }
+
+make_plot = function(D, x_arm="WV", min_prob, max_prob ){
   if( x_arm=="WV" ){ 
           xlab_label = 'Wave Height (Wave Velocity set to 300)'
           x_choice = "WV==300"
@@ -17,36 +86,22 @@ make_plot = function(D, x_arm="WV" ){
   } else {xlab_label = 'Wave Velocity (Wave Height set to 150)'
           x_choice = "WH==150"
           x = 'WV' }
-  PTR_ETnoD = D %>% filter_(x_choice, "count_or_prob=='prob'", "real_or_sim=='sim'") %>%
-              select(WH, WV, algo, ETnoD, PTR, ID, anion_approached_cation) %>%
-              mutate(WH = factor(WH), WV = factor(WV) ) %>%
-              mutate_(X=x) %>%
-              ggplot(aes(x=X, y=ETnoD, fill=algo, alpha =anion_approached_cation)) +
-              geom_boxplot() +
-              theme_minimal() +
-              xlab(xlab_label)+
-              theme(
-                axis.text.x = element_text(angle = 90, hjust = 1),
-                legend.position = 'top'
-              ) +
-              scale_y_continuous(labels=scales::percent) 
   
-  prob_react= D %>% filter_(x_choice, "count_or_prob=='prob'", "real_or_sim=='sim'") %>%
-              select(WH, WV, algo, ETnoD, PTR, ID, anion_approached_cation) %>%
-              mutate(WH = factor(WH), WV = factor(WV) ) %>%
-              mutate_(X=x) %>%
-              ggplot(aes(x=X, y=anion_approached_cation)) +
-              geom_boxplot()+
-              theme_minimal() +
-              xlab(xlab_label)+
-              theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-              scale_y_continuous(labels=scales::percent) 
+  PTR_ETnoD  = make_box_plot(D, xlab_label, x_choice, x)
+  prob_react = make_react_plot(D, xlab_label, x_choice, x)
+  error_plot = make_error_plot(D, xlab_label, x_choice, x, min_prob, max_prob)
   
-  list(PTR_ETnoD=PTR_ETnoD, prob_react=prob_react)
+  list( 
+    PTR_ETnoD = PTR_ETnoD, 
+    error     = error_plot, 
+    prob_react= prob_react 
+  )
 }
 
-plot_grid(plotlist=make_plot(D, "WV"), nrow=2)
-plot_grid(plotlist=make_plot(D, "WH"), nrow=2)
 
+min_prob = .025 
+max_prob = .975
 
-x_arm="WV"
+plot_grid(plotlist=make_plot(D, "WV", min_prob, max_prob), nrow=3)
+plot_grid(plotlist=make_plot(D, "WH", min_prob, max_prob), nrow=3)
+
