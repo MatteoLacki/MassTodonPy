929255618e1fb8e30a7a9989cc9cd202d6917a2f
Mateusz Łącki <matteo.lacki@gmail.com>
2017-06-21 23:52:27 +0200

    now really

--- Tests/in_silico/deconv_misc.py
+++ Tests/in_silico/deconv_misc.py
@@ -7,9 +7,6 @@ def change_key(seq, q, p, name):
     return name, q, p
 
 
-# storagePath = '/Users/matteo/Documents/MassTodon/MassTodonPy/Tests/data/substanceP_spectra.json'
-# with open(storagePath) as data_file:
-#     data = json.load(data_file)
 
 with open('data/sigmas_probs.json', 'r') as f:
     s2p = json.load(f)
