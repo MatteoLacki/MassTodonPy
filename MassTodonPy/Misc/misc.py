4cac2ec91d2f31fed77d837349d39a6365eab8b4
Mateusz Łącki <matteo.lacki@gmail.com>
2017-06-27 16:37:07 +0200

    trying to regain control over the stupid environment variable

--- MassTodonPy/Misc/misc.py
+++ MassTodonPy/Misc/misc.py
@@ -5,6 +5,7 @@ import os
 class cvxopt_wrapper(object):
     def __enter__(self):
         self.old = os.environ.get('OMP_NUM_THREADS', None)
+        print self.old
         os.environ['OMP_NUM_THREADS'] = "1"
         imp.reload(cvxopt)
 
--- MassTodonPy/__init__.py
+++ MassTodonPy/__init__.py
@@ -15,5 +15,7 @@
 #   You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
 #   Version 3 along with MassTodon.  If not, see
 #   <https://www.gnu.org/licenses/agpl-3.0.en.html>.
+import os
+os.environ['OMP_NUM_THREADS'] = "1"
 
 from .MassTodon import MassTodon, MassTodonize
