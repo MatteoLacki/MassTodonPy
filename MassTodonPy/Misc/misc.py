4f76e3927bebc001cb2128d1a12125bc91254a1c
Mateusz Łącki <matteo.lacki@gmail.com>
2017-06-27 15:57:14 +0200

    I do not know what is going on

--- MassTodonPy/Misc/misc.py
+++ MassTodonPy/Misc/misc.py
@@ -5,7 +5,6 @@ import os
 class cvxopt_wrapper(object):
     def __enter__(self):
         self.old = os.environ.get('OMP_NUM_THREADS', None)
-        print self.old
         os.environ['OMP_NUM_THREADS'] = "1"
         imp.reload(cvxopt)
 
