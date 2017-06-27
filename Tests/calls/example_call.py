bbb80f41ac9dfd0b8fc8e2396c396b26b60b58f8
Mateusz Łącki <matteo.lacki@gmail.com>
2017-06-27 16:27:59 +0200

    adjusting values

--- Tests/calls/example_call.py
+++ Tests/calls/example_call.py
@@ -13,13 +13,13 @@ opt_P  = .99
 M_minProb  = .7
 max_times_solve = 10
 
-# solver  = 'sequential'
-solver  = 'multiprocessing'
+solver  = 'sequential'
+# solver  = 'multiprocessing'
 multiprocesses_No = None
 
 method  = 'MSE'
 verbose = True
-bootstrap_repeats = 10
+bootstrap_repeats = 0
 
 T0 = time()
 res = MassTodonize( fasta           = mol['fasta'],
