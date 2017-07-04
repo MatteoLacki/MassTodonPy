1c64e50b9031c771291e40e1a030d980ed9e01e7
Mateusz Łącki <matteo.lacki@gmail.com>
2017-07-04 17:39:37 +0200

    adding the fragments plot

--- MassTodonPy/Visualization/highcharts.py
+++ MassTodonPy/Visualization/highcharts.py
@@ -4,19 +4,17 @@ from highcharts import Highchart
 def make_precursor_intensity_plot(precursors):
     H = Highchart(width=750, height=600)
     charges = precursors.keys()
-    Q_min = min(charges)
+    Q_min = 1
     Q_max = max(charges)
     categories = range(Q_min,Q_max+1)
     values = [ int(precursors[q]) for q in categories ]
     options = {
-    	'title': {
-            'text': 'Intensities Plot'
-        },
+    	'title': {'text': 'Total Intensity of Precursors'},
+        'subtitle':{'text': 'for different charge states'},
         'xAxis': {
             'categories': categories,
-            'title': {
-                'text': 'Charge State'
-            }
+            'title': { 'text': 'Charge State'},
+            'labels':{ 'format': 'q = {value}'}
         },
         'yAxis': {
             'min': 0,
@@ -24,22 +22,18 @@ def make_precursor_intensity_plot(precursors):
                 'text': 'Intensity',
                 'align': 'high'
             },
-            'labels': {
-                'overflow': 'justify'
-            }
+            'labels': {'overflow': 'justify'}
         },
         'plotOptions': {
-            'bar': {
-                'dataLabels': { 'enabled': True }
-            }
+            'bar': {'dataLabels': { 'enabled': True }}
         }
     }
     H.set_dict_options(options)
-    H.add_data_set(values, 'bar', 'MassTodonPy: Precursors Charge')
+    H.add_data_set(data=values,type='bar', name='Intensity')
     return H
 
-# H = make_precursor_intensity_plot(precursors)
-# H.save_file()
+# H = make_precursor_intensity_plot(precursors_intensitites)
+# H.save_file('/Users/matteo/Documents/MassTodon/MassTodonPy/Tests/IO/precursorCharge')
 # with open('/Users/matteo/Documents/MassTodon/MassTodonPy/Tests/IO/options.json','w') as h:
 #     json.dump(H.option, h)
 #
@@ -88,8 +82,8 @@ def make_etnod_ptr_probability_plot(res):
     return H
 
 # import json
-# H = make_etnod_ptr_probability_plot(res)
-# H.save_file()
+H = make_etnod_ptr_probability_plot(res)
+H.save_file('/Users/matteo/Documents/MassTodon/MassTodonPy/Tests/IO/etnod_ptr')
 # with open('/Users/matteo/Documents/MassTodon/MassTodonPy/Tests/IO/etnodPtr_options.json','w') as h:
 #     json.dump(H.option, h)
 #
@@ -150,64 +144,58 @@ def make_branching_ratio_plot(branching_ratios, branching_ratio, pk):
 #     json.dump(H.data, h)
 
 
-H = Highchart(width=750, height=600)
-categories = ['0-4', '5-9', '10-14', '15-19',
-  '20-24', '25-29', '30-34', '35-39', '40-44',
-  '45-49', '50-54', '55-59', '60-64', '65-69',
-  '70-74', '75-79', '80-84', '85-89', '90-94',
-  '95-99', '100 + ']
-
-options = {
-    'chart': {
-      'type': 'bar'
-    },
-    'title': {
-      'text': 'Population pyramid for Germany, 2015'
-    },
-    'subtitle': {
-      'text': 'Source: <a href="http://populationpyramid.net/germany/2015/">Population Pyramids of the World from 1950 to 2100</a>'
-    },
-    'xAxis': [{
-      'categories': categories,
-      'reversed': False,
-      'labels': {
-        'step': 1
-      }
-    }, {
-      'opposite': True,
-      'reversed': False,
-      'categories': categories,
-      'linkedTo': 0,
-      'labels': {
-        'step': 1
-      }
-    }],
-    'yAxis': {
-      'title': {'text': None},
-      'labels': {
-            'formatter': 'function () {\
-                return Math.abs(this.value) + "%";\
-            }'
+def make_fragmentation_prob_plot(fasta, res):
+    H = Highchart(width=2000, height=500)
+    fasta_list = [f for f in fasta]
+    options = {
+        'chart': {'type': 'column'},
+        'title': {'text': 'Probabilities of Fragmentation'},
+        'subtitle': {'text': 'as obtained by pairing ions by three different algorithms: basic, intermediate, and advanced.'},
+        'xAxis': {
+            'categories': fasta_list,
+            'crosshair': True,
+            'title':{'text':'Amino Acidic Sequence: N terminus to the left, C terminus to the right'}
+        },
+        'yAxis': {
+            'min': 0,
+            'title': {
+                'text': 'Probability of Fragmentation'
+            },
+            'labels':{
+                'format': '{value:.1f} %'
+            },
+        },
+        'tooltip': {
+            'valueDecimals': 1,
+            'headerFormat': '<table>',
+            'pointFormat': "<tr><td style='color:{series.color};padding:0'>{series.name}:</td>\
+                <td style='padding:0'><b>{point.y:.3f} %</b></td></tr>",
+            'footerFormat': '</table>',
+            'shared':     True,
+            'useHTML':  True
+        },
+        'plotOptions': {
+            'column': {
+                'pointPadding': 0.2,
+                'borderWidth': 0
+            }
         }
-    },
-    'plotOptions': {
-      'series': { 'stacking': 'normal' }
-    },
-    'tooltip': {
-        'formatter': 'function() {\
-        return "<b>" + this.series.name + ", age " + this.point.category + "</b><br/>" +\
-          "Population: " + Highcharts.numberFormat(Math.abs(this.point.y), 0);\
-      }'
-    }
-}
+     }
+    H.set_dict_options(options)
+    algos = ('basic_analysis', 'intermediate_analysis', 'advanced_analysis')
+    for algo in algos:
+        probs, counts = res[algo]
+        H.add_data_set(
+            data = [probs[i]*100 for i in xrange(len(fasta))],
+            name = algo.split('_')[0].title() ,
+            type = 'column'
+        )
+    return H
 
-H.set_dict_options(options)
-H.add_data_set( data = [-2.2, -2.2, -2.3, -2.5, -2.7, -3.1, -3.2, -3.0, -3.2, -4.3, -4.4, -3.6, -3.1, -2.4, -2.5, -2.3, -1.2, -0.6, -0.2, -0.0, -0.0],
-                type = 'bar',
-                name = 'Male'     )
-
-H.add_data_set( data = [2.1, 2.0, 2.2, 2.4, 2.6, 3.0, 3.1, 2.9, 3.1, 4.1, 4.3, 3.6, 3.4, 2.6, 2.9, 2.9,1.8, 1.2, 0.6, 0.1, 0.0 ],
-                type = 'bar',
-                name = 'Female'     )
-
-H.save_file('/Users/matteo/Documents/MassTodon/MassTodonPy/Tests/IO/age_pyramid')
+# H = make_fragmentation_prob_plot(fasta, res)
+# H.save_file('/Users/matteo/Documents/MassTodon/MassTodonPy/Tests/IO/fragmentation_probs')
+# with open('/Users/matteo/Documents/MassTodon/MassTodonPy/Tests/IO/fragmentation_probs_options.json','w') as h:
+#     json.dump(H.option, h)
+#
+# with open('/Users/matteo/Documents/MassTodon/MassTodonPy/Tests/IO/fragmentation_probs_data.json','w') as h:
+#     json.dump(H.data, h)
