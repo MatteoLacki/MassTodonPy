ed9bd199db92e93d2228c4ec75df7a691a6dc06b
MatteoLacki <matteo.lacki@gmail.com>
2017-07-14 14:13:46 +0200

    Cleansed the master version.

--- Tests/bootstrap/add_reaction_counts_to_plots.R
+++
@@ -1,9 +0,0 @@
-source('get_data.R')
-
-WH_150 = DD %>% filter(WH==150)
-
-get_algo = Vectorize(function(string) strsplit(string,'_')[[1]][1]) 
-check_if_precursor = Vectorize(function(string) tail(strsplit(string,'_')[[1]],1) == 'precursor') 
-
-find_among_colnames(WH_150, 'basic')
-
