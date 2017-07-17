6b986a745f408f34770a91bad830f56fffe0dd95
MatteoLacki <matteo.lacki@gmail.com>
2017-07-17 18:50:05 +0200

    Full results exposed to etdetective. Making webpage.

--- Tests/linking_to_ETDetective/ETDetective_IO.py
+++ Tests/linking_to_ETDetective/ETDetective_IO.py
@@ -3,7 +3,8 @@ from MassTodonPy import MassTodonize
 from MassTodonPy.TestScripts.substanceP import substanceP
 from MassTodonPy.TestScripts.ubiquitin  import ubiquitin
 from MassTodonPy.Outputing.to_etdetective import results_to_etdetective
-from time  import time
+from time import time
+from math import log
 
 mol = substanceP.copy()
 # mol = ubiquitin.copy()
@@ -11,7 +12,7 @@ mol = substanceP.copy()
 res = MassTodonize( fasta           = mol['fasta'],
                     precursor_charge= mol['Q'],
                     mz_prec         = .05,
-                    joint_probability_of_envelope = jP,
+                    joint_probability_of_envelope = .999,
                     modifications   = mol['modifications'],
                     spectrum        = mol['spectrum'],
                     opt_P           = .99,
@@ -23,4 +24,9 @@ res = MassTodonize( fasta           = mol['fasta'],
                     verbose         = False )
 
 
-results_to_etdetective(res['raw_estimates'], mol['fasta'], mol['modifications'])
+results_4_etdetective = results_to_etdetective(res, mol['fasta'], mol['modifications'])
+
+
+probs, counter = res['basic_analysis']
+
+-log(probs['no fragmentation'])/mol['Q']**2
