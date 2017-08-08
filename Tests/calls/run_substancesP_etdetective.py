74678dc3f67abdd3d4f8247dc8ae7124619cb51f
Mateusz Łącki <matteo.lacki@gmail.com>
2017-08-08 18:13:25 +0200

    I don't remember what I did here :D

--- Tests/calls/run_substancesP_etdetective.py
+++ Tests/calls/run_substancesP_etdetective.py
@@ -7,9 +7,6 @@ from MassTodonPy.TestScripts.substancesP    import substancesP
 from MassTodonPy.Outputing.to_etdetective   import results_to_etdetective
 
 results = []
-
-
-
 # mol = substancesP[0]; ID = 0
 for ID, mol in enumerate(substancesP):
     fasta= mol['fasta']
@@ -28,15 +25,23 @@ for ID, mol in enumerate(substancesP):
                         verbose         = False )
 
     mol['experimental_setting']['Q'] = Q
-    results.append({
+    WH, WV = [mol['experimental_setting'][x] for x in ('WH','WV')]
+    mol['experimental_setting']['files'] = 'ID'+str(ID)+'_'+'WH'+str(WH)+'_'+'WV'+str(WV)
+
+    etdetective_input = {
         'masstodon_output':         res,
         'experimental_setting':     mol['experimental_setting'],
         'etdetective_input':        results_to_etdetective( masstodon_results = res,
                                                             fasta = fasta,
                                                             Q = Q,
-                                                            modifications = mol['modifications'] ) })
+                                                            modifications = mol['modifications'] ) }
 
+    etdetective_input['etdetective_input']['fasta'] = '*'+fasta+'*'
+    results.append(etdetective_input)
     print "Dumped", ID+1, "out of", len(substancesP)
 
 with open('/Users/matteo/Documents/MassTodon/MassTodonPy/Tests/Outputs/ETDetective/subsP.masstodon', 'w') as h:
     pickle.dump(results, h, protocol=pickle.HIGHEST_PROTOCOL)
+
+# from collections import Counter
+# [s['experimental_setting'] for s in substancesP]
