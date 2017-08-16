28c98e0d564014cd85ce96025f79274368d28601
Mateusz Łącki <matteo.lacki@gmail.com>
2017-08-16 17:12:28 +0200

    maybe now

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         'Programming Language :: Python :: 2.7'],
     install_requires=[ 'linearCounter', 'numpy', 'pandas', 'pyteomics>=3.4.1', 'lxml',
         'cvxopt', 'IsoSpecPy', 'scipy', 'networkx', 'intervaltree', 'pyinterval',
-        'pandas', 'python-highcharts'],
+        'python-highcharts'],
     scripts=['bin/masstodon', 'bin/masstodon_example_call'],
     include_package_data = True,
     package_data={
