3381f0a48b53750ba04c1bdf4617a9621a710e1c
Mateusz Łącki <matteo.lacki@gmail.com>
2017-08-16 15:55:39 +0200

    mock added

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,12 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../MassTodonPy'))
 
+import mock
+
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate',
+'linearCounter', 'pandas', 'pyteomics>=3.4.1', 'lxml', 'cvxopt', 'IsoSpecPy', 'networkx', 'intervaltree', 'pyinterval','python-highcharts']
+for mod_name in MOCK_MODULES:
+sys.modules[mod_name] = mock.Mock()
 
 # -- General configuration ------------------------------------------------
 
