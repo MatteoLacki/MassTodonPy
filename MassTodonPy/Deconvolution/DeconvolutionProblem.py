b7c34176db08cfbd178760d8e1f642cb03b62e6d
Matteo Lacki <matteo.lacki@gmail.com>
2018-02-10 12:26:24 +0100

    testing try with cvxopt

--- MassTodonPy/Deconvolution/DeconvolutionProblem.py
+++ MassTodonPy/Deconvolution/DeconvolutionProblem.py
@@ -173,16 +173,15 @@ class DeconvolutionProblem(nx.Graph):
         iteration = 1
         while not stop or iteration <= self.max_times:
             setseed(randint(0, 1000000))
-            try:
-                self.solution = solvers.qp(self.P, self.q, self.G,
-                                           self.h, self.A, self.b,
-                                           initvals=self.initvals)
-                if self.solution['status'] is 'optimal':
-                    stop = True
-                    solved = True
-            except ValueError as e:
-                solved = False
-                print(e)
+            # try:
+            self.solution = solvers.qp(self.P, self.q, self.G,
+                                       self.h, self.A, self.b,
+                                       initvals=self.initvals)
+            if self.solution['status'] is 'optimal':
+                stop = True
+                solved = True
+            # except ValueError as e:
+            #     pass
             iteration += 1
         if not solved:
             print("Tried {} times and no optimum reached.".format(self.max_times))
