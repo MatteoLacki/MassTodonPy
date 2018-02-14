28c8ed68e2b208726b9e182425c18a861c714d29
Matteo Lacki <matteo.lacki@gmail.com>
2018-02-14 23:03:45 +0100

    Need to get rid of environ error while calling cvxopt_wrapper with py2.

--- MassTodonPy/test_masstodon.py
+++ MassTodonPy/test_masstodon.py
@@ -1,22 +1,46 @@
 from __future__ import absolute_import, division, print_function
-from collections import Counter, namedtuple
+from collections import defaultdict, Counter
 import unittest
 
 from MassTodonPy.Data.get_dataset import get_dataset
-from MassTodonPy.MatchMaker.SimpleCzMatch import SimpleCzMatch
-from MassTodonPy.MatchMaker.CzMatch import CzMatch
-from MassTodonPy.Reporter.misc import Brick
-
-# TODO add test for the optimality of the fragment matching
-
-def node_to_tuple(node):
-    return node.type + str(node.no), node.q
-
-class VoidClass(object):
-    pass
+from MassTodonPy.MassTodon import MassTodon
 
 
-# no alphas now: rewrite test using graphs
+class TestMassTodonOnSubstanceP(unittest.TestCase):
+    def setUp(self):
+        """Load data on Substance P."""
+
+        substanceP = get_dataset('substanceP')
+        modifications = defaultdict(dict)
+        for (number, group), mods in substanceP.precursor.modifications.items():
+            modifications[number][group] = dict(mods)
+        
+        self.args = {'spectrum':        substanceP.spectrum,
+                     'min_intensity':   50.0,
+                     'fasta':           substanceP.precursor.fasta,
+                     'name':            'substanceP',
+                     'modifications':   modifications,
+                     'charge':          3,
+                     'mz_tol':          .05}
+        
+    def run_masstodon(self):
+        """Run one session of the MassTodon on the Substance P data."""
+        masstodon = MassTodon(**self.args)
+
+    def test_some_times(self):
+        """Test the MassTodon on substance P, M times."""
+        M = 100
+        res = Counter() 
+        for i in range(M):
+            try:
+                masstodon = self.run_masstodon()
+                res['success'] += 1
+            except ValueError as e:
+                res['failure'] += 1
+        
+        self.assertEqual(res['success'], M)
+        self.assertEqual(res['failure'], 0)
+                                                  
 #class TestPeakPicker(unittest.TestCase):
 #    def setUp(self):
 #        """Set up a method."""
