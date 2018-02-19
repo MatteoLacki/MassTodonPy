d38f50994824341ca8ef04b775685f123d51ba43
Matteo Lacki <matteo.lacki@gmail.com>
2018-02-19 11:59:47 +0100

    It seems that python2 is more stable with cvxopt.

--- MassTodonPy/Misc/io.py
+++ MassTodonPy/Misc/io.py
@@ -1,13 +1,9 @@
 import os
-
 from MassTodonPy.Parsers.Paths import parse_path
 
 
 def create_folder_if_needed(path):
     """Creates folder according to the path, if necessary."""
     file_path, file_name, file_ext = parse_path(path)
-    try:
-        if not os.path.exists(file_path):
-            os.makedirs(file_path)
-    except AttributeError:
-        print(os.path)
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
