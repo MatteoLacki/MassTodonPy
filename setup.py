18321bc664b02e3e393f465dab69e1424991e232
Matteo Lacki <matteo.lacki@gmail.com>
2018-03-04 17:44:53 +0100

    trying to get a release branch.

--- setup.py
+++ setup.py
@@ -21,14 +21,14 @@ from setuptools import setup, find_packages
 setup(
     name='MassTodonPy',
     packages=find_packages(),
-    version='0.3.3',
+    version='0.3.4',
     description='Estimate the products of Electron Transfer Dissociation in \
     Mass Spectrometry for a given biological substance and \
     the chemical reaction probabilities that lead to these products.',
     author=u'Mateusz Krzysztof Łącki',
     author_email='matteo.lacki@gmail.com',
     url='https://github.com/MatteoLacki/MassTodonPy',
-    download_url='https://github.com/MatteoLacki/MassTodonPy/archive/0.3.3.tar.gz',
+    download_url='https://github.com/MatteoLacki/MassTodonPy/archive/0.3.4.tar.gz',
     keywords=[
         'Mass Spectrometry',
         'ETD',
